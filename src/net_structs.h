be356513ec53ecafa97fa7982c406c529814515c
david <david@weinman.com>
2013-08-21 23:44:33 -0700

    initial commit
    added a few headers and a .c, these are the guts of what I came up with
    during the past two days (mon,tues,wed) since reading ch0x400 in HTAE.
    
    This was largely inspired by the "decode_sniff.c" example in Ch0x400 in
    HTAE by Jon Erikson.

---
+++ src/net_structs.h
@@ -0,0 +1,71 @@
+/* net_structs.h
+ * written by: David Weinman
+ * last update: 08/21/13
+ * */
+
+/* note: this code was heavily influenced by Jon Erikson's
+   'Hacking: The Art of Exploitation' */
+
+/* Ethernet Header */
+
+// ------ constants
+
+#define ETH_ADDR_LEN 6
+#define ETH_HDR_LEN 14
+
+// ------ definitions
+
+struct eth_hdr {
+
+	unsigned char dest_eth_addr[ETH_ADDR_LEN]; // Destination MAC address
+	unsigned char src_eth_addr[ETH_ADDR_LEN]; // Source MAC address
+
+	unsigned short ether_type; // Type of Ethernet packet
+
+};
+
+/* Internet Protocol Header */
+
+struct ip_hdr {
+
+	unsigned char ip_version_and_header_length; // version and header length combined
+	unsigned char ip_tos;          // type of service
+	unsigned short ip_len;         // total length
+	unsigned short ip_id;          // identification number
+	unsigned short ip_frag_offset; // fragment offset and flags
+	unsigned char ip_ttl;          // time to live
+	unsigned char ip_type;         // protocol type
+	unsigned short ip_checksum;    // checksum
+	unsigned int ip_src_addr;      // source IP address
+	unsigned int ip_dest_addr;     // destination IP address
+
+};
+
+/* Transmission Control Protocol Header */
+
+struct tcp_hdr {
+
+	unsigned short tcp_src_port;
+	unsigned short tcp_dest_port;
+	unsigned int tcp_seq;
+	unsigned int tcp_ack;
+	unsigned char reserved:4; // 4 bits fom the 6 of reserved space
+	unsigned char tcp_offset:4; // TCP data offset for little-endian host
+	unsigned char tcp_flags; // TCP flags (and 2 bits from reserved space)
+
+	#define TCP_FIN 0x01
+	#define TCP_SYN 0x02
+	#define TCP_RST 0x04
+	#define TCP_PUSH 0x08
+	#define TCP_ACK 0x10
+	#define TCP_URG 0x20
+
+	unsigned short tcp_window; // TCP window size
+	unsigned short tcp_checksum;
+	unsigned short tcp_urgent; // TCP urgent pointer
+
+};
+
+
+
+
---
+++ src/scan_detector.c
@@ -0,0 +1,142 @@
+/* scan_detector.c
+ * written by: David Weinman
+ * last update: 08/16/13
+ * */
+
+#include <stdio.h>
+#include <pcap/pcap.h>
+
+#include "malloc_dump.h"
+#include "net_structs.h"
+
+
+void scan_fatal(const char *, const char *);
+void main(int, char **);
+void caught_packet(u_char*, const struct pcap_pkthdr*, const u_char*);
+int isSYNPkt(const u_char*);
+
+// an error function
+void scan_fatal(const char *failed_in, const char *errbuf) {
+
+        printf("Fatal Error in %s:", failed_in);
+	fatal((char *) errbuf);
+        exit(1);
+
+}
+
+
+//
+void main(int argc, char ** argv) {
+
+	int i = 0;
+        struct pcap_pkthdr cap_header;
+        const u_char *packet, *pkt_data;
+        char errbuf[PCAP_ERRBUF_SIZE];
+        char *device;
+	pcap_t *pcap_handle;
+
+	device = pcap_lookupdev(errbuf);
+
+	if (device == NULL) {
+		scan_fatal("pcap_lookupdev", errbuf);
+	}
+
+	pcap_handle = pcap_open_live(device, 4096, 1, 0, errbuf);
+
+	while (i++ < atoi(argv[1])) {
+
+		printf("\n\npkt # %d --\n", i);
+		//printf("\n1\n\n");
+		pcap_loop(pcap_handle, 5, caught_packet, NULL);
+		pcap_close(pcap_handle);
+	
+	}
+
+	return;
+
+}
+
+// 
+void caught_packet(u_char *user_args, const struct pcap_pkthdr *cap_header, const u_char *packet) {
+
+	const struct ip_hdr *ip_header = (const struct ip_hdr *)packet;
+	const struct tcp_hdr *tcp_header = (const struct tcp_hdr *)packet;
+	int tcp_header_length, total_header_size, pkt_data_len;
+	int header_size = 4 * tcp_header->tcp_offset;
+	u_char *pkt_data;
+
+	printf("\n1\n\n");
+
+	if (isSYNPkt(packet+ETH_HDR_LEN+sizeof(struct ip_hdr))) {
+
+		
+        	tcp_header_length = packet+ETH_HDR_LEN+sizeof(struct ip_hdr);
+	        total_header_size = ETH_HDR_LEN+sizeof(struct ip_hdr)+tcp_header_length;
+
+		pkt_data = (u_char *)packet + total_header_size;
+		pkt_data_len = cap_header->len - total_header_size;
+
+		printf("\nsrc: %d  |  dst: %d\n", inet_ntoa(ip_header->ip_src_addr), inet_ntoa(ip_header->ip_dest_addr));
+		printf("\ntype: %u\n", (u_int) ip_header->ip_type);
+		printf("\nID: %hu\tLength: %hu )\n", ntohs(ip_header->ip_id), ntohs(ip_header->ip_len));
+
+		dump(pkt_data, pkt_data_len);
+
+	}
+
+}
+
+int isSYNPkt(const u_char *header_start) {
+
+	const struct tcp_hdr *tcp_header = (const struct tcp_hdr *)header_start;
+	int header_size = 4 * tcp_header->tcp_offset;
+
+	if (tcp_header->tcp_flags & TCP_FIN) return 0;
+
+	if (tcp_header->tcp_flags & TCP_URG) return 0; 
+	
+	if (tcp_header->tcp_flags & TCP_RST) return 0;
+
+	if (tcp_header->tcp_flags & TCP_PUSH) return 0;
+
+	if (tcp_header->tcp_flags & TCP_ACK) return 0;
+
+	return tcp_header->tcp_flags & TCP_SYN;
+
+}
+
+/*
+void decode_eth(char *printString, const u_char *packet) {
+
+	int i;
+	const struct eth_hdr *ethernet_header;
+
+	ethernet_header = (const struct eth_hdr *) header_start;
+	snprintf(printString, MAX_PRINT_LEN, "[[  Layer 2 :: Ethernet Header  ]]\n[ Source: %02x", ethernet_header->ether_src_addr[0]);
+
+}*/
+
+/* PSEUDO CODE
+
+pkArr = define const size array for packets
+
+while true {
+
+	for (i = 0; i < 10; i++) {
+
+		recieve a tcp packet
+		append it to pkArr
+		if packet has only SYN up {
+			recieve a packet
+			if packet has SYN and RST up {
+				print detection and dump packets
+			}
+		}
+
+	}
+
+}
+
+*/
+
+
