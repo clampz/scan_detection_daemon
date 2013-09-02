c6113b0fc6c12030cfe6e3c5b040c639fffa2a24
david <david@weinman.com>
2013-09-01 18:57:56 -0700

    added bash script and network adaptor name printout program
    i wanted to make the scan detector find the non-loopback ip addr, so
    the bash code along with a network adaptor name printout program should
    do the job.

---
+++ src/printDev.c
@@ -0,0 +1,34 @@
+/* printDev.c
+ * written by: David Weinman
+ * last update: 08/21/13
+ * */
+
+// prints the id for the current default network adapter
+
+#include <pcap/pcap.h>
+#include "malloc_dump.h"
+
+int main(int, char **);
+
+int main(int argc, char **argv) {
+
+	char errbuf[PCAP_ERRBUF_SIZE];
+        char *device;
+
+        if (argc != 1) {printf("\nwrong # of args.\n\n"); exit(1);}
+
+// find a device using packet capture lib
+        device = pcap_lookupdev(errbuf);
+
+// if no device is found, print an error
+        if (device == NULL) {
+                fatal(errbuf);
+        }
+
+// print the id
+	printf("%s\n", device);
+
+	return 1;
+
+}
+
