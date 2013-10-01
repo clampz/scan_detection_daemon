bee3cc484f8557542ed0f620b801a8b4fcf86a03
David Weinman <david@weinman.com>
2013-10-01 14:20:34 -0700

    updated net_structs with more consistency of style

--- src/net_structs.h
+++ src/net_structs.h
@@ -65,12 +65,12 @@ struct tcp_hdr {
 	unsigned char tcp_offset:4; // TCP data offset for little-endian host
 	unsigned char tcp_flags; // TCP flags (and 2 bits from reserved space)
 
-	#define TCP_FIN 0x01
-	#define TCP_SYN 0x02
-	#define TCP_RST 0x04
-	#define TCP_PUSH 0x08
-	#define TCP_ACK 0x10
-	#define TCP_URG 0x20
+#define TCP_FIN 0x01
+#define TCP_SYN 0x02
+#define TCP_RST 0x04
+#define TCP_PUSH 0x08
+#define TCP_ACK 0x10
+#define TCP_URG 0x20
 
 	unsigned short tcp_window; // TCP window size
 	unsigned short tcp_checksum;
